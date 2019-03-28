5b0255b16fe2ce8b0e64351e2824fdea5d4794d7
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-03-28 16:22:32 +0300

    feedback - fix

--- events/feedback/js/FeedbackForm.js
+++ events/feedback/js/FeedbackForm.js
@@ -14,40 +14,40 @@ const FeedbackForm = ({data, onSubmit}) => {
     <p>Чем мы можем помочь?</p>
   </div>
   <div className="contact-form__input-group">
-    <input className="contact-form__input contact-form__input--radio" id="salutation-mr" name="salutation" type="radio" value="Мистер"/>
+    <input className="contact-form__input contact-form__input--radio" onChange={onChange} id="salutation-mr" name="salutation" type="radio" value="Мистер"/>
     <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">Мистер</label>
-    <input className="contact-form__input contact-form__input--radio" id="salutation-mrs" name="salutation" type="radio" value="Мисис"/>
+    <input className="contact-form__input contact-form__input--radio" onChange={onChange} id="salutation-mrs" name="salutation" type="radio" value="Мисис" checked/>
     <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mrs">Мисис</label>
-    <input className="contact-form__input contact-form__input--radio" id="salutation-ms" name="salutation" type="radio" value="Мис"/>
+    <input className="contact-form__input contact-form__input--radio" onChange={onChange} id="salutation-ms" name="salutation" type="radio" value="Мис"/>
     <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-ms">Мис</label>
   </div>
   <div className="contact-form__input-group">
     <label className="contact-form__label" htmlFor="name">Имя</label>
-    <input className="contact-form__input contact-form__input--text" id="name" name="name" type="text"  value={data.name}/>
+    <input className="contact-form__input contact-form__input--text" onChange={onChange} id="name" name="name" type="text"  value={data.name}/>
   </div>
   <div className="contact-form__input-group">
     <label className="contact-form__label" htmlFor="email">Адрес электронной почты</label>
-    <input className="contact-form__input contact-form__input--email" id="email" name="email" type="email" value={data.email}/>
+    <input className="contact-form__input contact-form__input--email" onChange={onChange} id="email" name="email" type="email" value={data.email}/>
   </div>
   <div className="contact-form__input-group">
     <label className="contact-form__label" htmlFor="subject">Чем мы можем помочь?</label>
-    <select className="contact-form__input contact-form__input--select" id="subject" name="subject" value={data.subject}>
+    <select className="contact-form__input contact-form__input--select" onChange={onChange} id="subject" name="subject" value={data.subject}>
       <option>У меня проблема</option>
       <option>У меня важный вопрос</option>
     </select>
   </div>
   <div className="contact-form__input-group">
     <label className="contact-form__label" htmlFor="message">Ваше сообщение</label>
-    <textarea className="contact-form__input contact-form__input--textarea" id="message" name="message" rows="6" cols="65"  value={data.message}></textarea>
+    <textarea className="contact-form__input contact-form__input--textarea" onChange={onChange} id="message" name="message" rows="6" cols="65"  value={data.message}></textarea>
   </div>
   <div className="contact-form__input-group">
     <p className="contact-form__label--checkbox-group">Хочу получить:</p>
-    <input className="contact-form__input contact-form__input--checkbox" id="snacks-pizza" name="snacks" type="checkbox" value="пицца"/>
+    <input className="contact-form__input contact-form__input--checkbox" onChange={onChange} id="snacks-pizza" name="snacks" type="checkbox" value="пицца" checked/>
     <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
-    <input className="contact-form__input contact-form__input--checkbox" id="snacks-cake" name="snacks" type="checkbox" value="пирог"/>
+    <input className="contact-form__input contact-form__input--checkbox" onChange={onChange} id="snacks-cake" name="snacks" type="checkbox" value="пирог"/>
     <label clclassNameass="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">Пирог</label>
   </div>
-  <button className="contact-form__button" type="submit">Отправить сообщение!</button>
+  <button className="contact-form__button" onSubmit={onSubmit} type="submit">Отправить сообщение!</button>
   <output id="result" />
 </form>
   );
