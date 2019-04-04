facdf6baafab546e87c826b43524dea037d66a16
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-04-04 14:00:52 +0300

    fixes of charts

--- composition/chart/js/App.js
+++ composition/chart/js/App.js
@@ -1,159 +1,150 @@
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function compareNumbers(a, b) {
-  return a - b;
+	return a - b;
 }
 
-const Charts = (props) => {
-
+const Charts = props => {
 	let value = '';
-
-	
-
 	return (
 		<div className={`Charts ${props.dir}`}>
-          {props.data.map((serie, serieIndex) => {
-            var sortedSerie = serie.slice(0),
-              sum;
+			{props.data.map((serie, serieIndex) => {
+				var sortedSerie = serie.slice(0),
+					sum;
 
-            sum = serie.reduce((carry, current) => carry + current, 0);
-						sortedSerie.sort(compareNumbers);
-						
-						switch(props.type) {
-							case 'stacked':
-							value = sum;
-							break;
-							default:
-							value = props.max;
-						}
+				sum = serie.reduce((carry, current) => carry + current, 0);
+				sortedSerie.sort(compareNumbers);
 
-						return (<ChartsSerie 
-							type={props.type}
-							sortedSerie={sortedSerie}
-							serieIndex={serieIndex} 
-							max={props.max}
-							labels={props.labels} 
-							serie={serie} 
-							colors={props.colors}
-							value={value}
-							height={props.height}
-						/>);
-          })}
-        </div>
+				switch (props.type) {
+					case 'stacked':
+						value = sum;
+						break;
+					default:
+						value = props.max;
+				}
+
+				return (
+					<ChartsSerie
+						type={props.type}
+						sortedSerie={sortedSerie}
+						serieIndex={serieIndex}
+						max={props.max}
+						labels={props.labels}
+						serie={serie}
+						colors={props.colors}
+						value={value}
+						height={props.height}
+					/>
+				);
+			})}
+		</div>
 	);
 };
 
-const ChartsItem = (props) => {
-  return (
-    <div className={`Charts--item ${props.type}`} style={props.style} key={props.itemIndex}>
-      <b style={{ color: props.color }}>{props.item}</b>
-    </div>
-  );
+const ChartsItem = props => {
+	return (
+		<div className={`Charts--item ${props.type}`} style={props.style} key={props.itemIndex}>
+			<b style={{ color: props.color }}>{props.item}</b>
+		</div>
+	);
 };
 
-const ChartsSerie = (props) => {
-  return (
-    <div
-      className={`Charts--serie ${props.type}`}
-      key={props.serieIndex}
-      style={{ height: props.height }}
-    >
-      <label>{props.labels[props.serieIndex]}</label>
-      {props.serie.map((item, itemIndex) => {
-        var color = props.colors[itemIndex],
-          style,
+const ChartsSerie = props => {
+	return (
+		<div className={`Charts--serie ${props.type}`} key={props.serieIndex} style={{ height: props.height }}>
+			<label>{props.labels[props.serieIndex]}</label>
+			{props.serie.map((item, itemIndex) => {
+				var color = props.colors[itemIndex],
+					style,
 					size = (item / props.value) * 100;
-							
-					style = {
-						backgroundColor: color,
-						opacity: item / props.max + 0.05,
-						zIndex: item,
-						height: size + "%"
-					};
-					
-					if (props.type == 'layered') {
-						style = Object.assign(style, {right: (props.sortedSerie.indexOf(item) / (props.serie.length + 1)) * 100 + "%"});
-					}
 
-					if (props.height == 'auto') {
-						style.height = null;
-						style.width = size + "%";
-					}
+				style = {
+					backgroundColor: color,
+					opacity: item / props.max + 0.05,
+					zIndex: item,
+					height: size + '%',
+				};
 
-        return <ChartsItem type={props.type} item={item} itemIndex={itemIndex} color={color} style={style} />;
-      })}
-    </div>
-  );
+				if (props.type == 'layered') {
+					style = Object.assign(style, {
+						right: (props.sortedSerie.indexOf(item) / (props.serie.length + 1)) * 100 + '%',
+					});
+				}
+
+				if (props.height == 'auto') {
+					style.height = null;
+					style.width = size + '%';
+				}
+
+				return <ChartsItem type={props.type} item={item} itemIndex={itemIndex} color={color} style={style} />;
+			})}
+		</div>
+	);
 };
 
-const Legend = (props) => {
+const Legend = props => {
 	return (
 		<div className="Legend">
-          {props.labels.map((label, labelIndex) => {
-            return (
-              <div>
-                <span
-                  className="Legend--color"
-                  style={{
-                    backgroundColor: props.colors[labelIndex % props.colors.length]
-                  }}
-                />
-                <span className="Legend--label">{label}</span>
-              </div>
-            );
-          })}
-        </div> 
-
+			{props.labels.map((label, labelIndex) => {
+				return (
+					<div>
+						<span
+							className="Legend--color"
+							style={{
+								backgroundColor: props.colors[labelIndex % props.colors.length],
+							}}
+						/>
+						<span className="Legend--label">{label}</span>
+					</div>
+				);
+			})}
+		</div>
 	);
 };
 
+const propsCharts = {	
+	data: [],
+	series: ['France', 'Italy', 'England', 'Sweden', 'Germany'],
+	labels: ['cats', 'dogs', 'horses', 'ducks', 'cows'],
+	colors: ['#43A19E', '#7B43A1', '#F2317A', '#FF9824', '#58CF6C']};
+
 class App extends React.Component {
-  componentWillMount() {
-    this.setState({
-      data: [],
-      series: ["France", "Italy", "England", "Sweden", "Germany"],
-      labels: ["cats", "dogs", "horses", "ducks", "cows"],
-      colors: ["#43A19E", "#7B43A1", "#F2317A", "#FF9824", "#58CF6C"]
-    });
-  }
+	componentWillMount() {
+		this.setState(propsCharts);
+	}
 
-  componentDidMount() {
-    this.populateArray();
-    setInterval(this.populateArray.bind(this), 10000);
-  }
+	componentDidMount() {
+		this.populateArray();
+		setInterval(this.populateArray.bind(this), 10000);
+	}
 
-  populateArray() {
-    const series = 5;
-    const serieLength = 5;
+	populateArray() {
+		const series = 5;
+		const serieLength = 5;
 
-    let data = new Array(series).fill(new Array(serieLength).fill(0));
-    data = data.map(serie => serie.map(item => getRandomInt(0, 20)));
+		let data = new Array(series).fill(new Array(serieLength).fill(0));
+		data = data.map(serie => serie.map(item => getRandomInt(0, 20)));
 
-    this.setState({ data });
-  }
+		this.setState({ data });
+	}
 
-  render() {
-    const { data, colors, labels, series } = this.state;
-    const max = data.reduce(
-      (max, serie) =>
-        Math.max(
-          max,
-          serie.reduce((serieMax, item) => Math.max(serieMax, item), 0)
-        ),
-      0
-    );
+	render() {
+		const { data, colors, labels, series } = this.state;
+		const max = data.reduce(
+			(max, serie) => Math.max(max, serie.reduce((serieMax, item) => Math.max(serieMax, item), 0)),
+			0
+		);
 
-    return (
-      <section>
+		return (
+			<section>
 				<Charts max={max} data={data} colors={colors} labels={labels} height={250} />
 				<Charts type={'stacked'} max={max} data={data} colors={colors} labels={labels} height={250} />
 				<Charts type={'layered'} max={max} data={data} colors={colors} labels={labels} height={250} />
-				<Charts dir="horizontal" max={max} data={data} colors={colors} labels={labels} height={'auto'} />
-				<Legend labels={labels} colors={colors}/>
-  
-      </section>
-    );
-  }
+				<Charts dir="horizontal" max={max} data={data} colors={colors} labels={series} height={'auto'} />
+				<Legend labels={labels} colors={colors} />
+			</section>
+		);
+	}
 }
--- events/feedback/js/FeedbackForm.js
+++ events/feedback/js/FeedbackForm.js
@@ -1,98 +1,182 @@
 /******************************
  * Ваша реализация компонента
  *****************************/
-const FeedbackForm = ({data, onSubmit}) => {
-  
-  
-  function onChange(event) {
-    data.onChange(event.target.value);
-  }
 
+const FeedbackForm = ({ data, onSubmit }) => {
+	function onChange(event) {
+		data.onChange(event.target.value);
+	}
+  
+  //let salutationEl, nameEl, emailEl, subjectEl, messageEl, snacksEl;
+  
   return (
-    <form class="content__form contact-form">
-  <div class="testing">
-    <p>Чем мы можем помочь?</p>
-  </div>
-  <div className="contact-form__input-group">
-    <input className="contact-form__input contact-form__input--radio" onChange={onChange} id="salutation-mr" name="salutation" type="radio" value="Мистер"/>
-    <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">Мистер</label>
-    <input className="contact-form__input contact-form__input--radio" onChange={onChange} id="salutation-mrs" name="salutation" type="radio" value="Мисис" checked/>
-    <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mrs">Мисис</label>
-    <input className="contact-form__input contact-form__input--radio" onChange={onChange} id="salutation-ms" name="salutation" type="radio" value="Мис"/>
-    <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-ms">Мис</label>
-  </div>
-  <div className="contact-form__input-group">
-    <label className="contact-form__label" htmlFor="name">Имя</label>
-    <input className="contact-form__input contact-form__input--text" onChange={onChange} id="name" name="name" type="text"  value={data.name}/>
-  </div>
-  <div className="contact-form__input-group">
-    <label className="contact-form__label" htmlFor="email">Адрес электронной почты</label>
-    <input className="contact-form__input contact-form__input--email" onChange={onChange} id="email" name="email" type="email" value={data.email}/>
-  </div>
-  <div className="contact-form__input-group">
-    <label className="contact-form__label" htmlFor="subject">Чем мы можем помочь?</label>
-    <select className="contact-form__input contact-form__input--select" onChange={onChange} id="subject" name="subject" value={data.subject}>
-      <option>У меня проблема</option>
-      <option>У меня важный вопрос</option>
-    </select>
-  </div>
-  <div className="contact-form__input-group">
-    <label className="contact-form__label" htmlFor="message">Ваше сообщение</label>
-    <textarea className="contact-form__input contact-form__input--textarea" onChange={onChange} id="message" name="message" rows="6" cols="65"  value={data.message}></textarea>
-  </div>
-  <div className="contact-form__input-group">
-    <p className="contact-form__label--checkbox-group">Хочу получить:</p>
-    <input className="contact-form__input contact-form__input--checkbox" onChange={onChange} id="snacks-pizza" name="snacks" type="checkbox" value="пицца" checked/>
-    <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
-    <input className="contact-form__input contact-form__input--checkbox" onChange={onChange} id="snacks-cake" name="snacks" type="checkbox" value="пирог"/>
-    <label clclassNameass="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">Пирог</label>
-  </div>
-  <button className="contact-form__button" onSubmit={onSubmit} type="submit">Отправить сообщение!</button>
-  <output id="result" />
-</form>
-  );
-  
+		<form class="content__form contact-form">
+			<div class="testing">
+				<p>Чем мы можем помочь?</p>
+			</div>
+			<div className="contact-form__input-group">
+				<input
+					className="contact-form__input contact-form__input--radio"
+					onChange={onChange}
+					id="salutation-mr"
+					name="salutation"
+					type="radio"
+					value="Мистер"
+				/>
+				<label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">
+					Мистер
+				</label>
+				<input
+					className="contact-form__input contact-form__input--radio"
+					onChange={onChange}
+					id="salutation-mrs"
+					name="salutation"
+					type="radio"
+					value="Мисис"
+					defaultChecked
+				/>
+				<label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mrs">
+					Мисис
+				</label>
+				<input
+					className="contact-form__input contact-form__input--radio"
+					onChange={onChange}
+					id="salutation-ms"
+					name="salutation"
+					type="radio"
+					value="Мис"
+				/>
+				<label className="contact-form__label contact-form__label--radio" htmlFor="salutation-ms">
+					Мис
+				</label>
+			</div>
+			<div className="contact-form__input-group">
+				<label className="contact-form__label" htmlFor="name">
+					Имя
+				</label>
+				<input
+					className="contact-form__input contact-form__input--text"
+					onChange={onChange}
+					id="name"
+					name="name"
+					type="text"
+					defaultValue={data.name}
+				/>
+			</div>
+			<div className="contact-form__input-group">
+				<label className="contact-form__label" htmlFor="email">
+					Адрес электронной почты
+				</label>
+				<input
+					className="contact-form__input contact-form__input--email"
+					onChange={onChange}
+					id="email"
+					name="email"
+					type="email"
+					defaultValue={data.email}
+				/>
+			</div>
+			<div className="contact-form__input-group">
+				<label className="contact-form__label" htmlFor="subject">
+					Чем мы можем помочь?
+				</label>
+				<select
+					className="contact-form__input contact-form__input--select"
+					onChange={onChange}
+					defaultValue={1}
+					id="subject"
+					name="subject"
+				>
+					<option value="0">У меня проблема</option>
+					<option value="1">У меня важный вопрос</option>
+				</select>
+			</div>
+			<div className="contact-form__input-group">
+				<label className="contact-form__label" htmlFor="message">
+					Ваше сообщение
+				</label>
+				<textarea
+					className="contact-form__input contact-form__input--textarea"
+					onChange={onChange}
+					defaultValue={data.message}
+					id="message"
+					name="message"
+					rows="6"
+					cols="65"
+				/>
+			</div>
+			<div className="contact-form__input-group">
+				<p className="contact-form__label--checkbox-group">Хочу получить:</p>
+				<input
+					className="contact-form__input contact-form__input--checkbox"
+					onChange={onChange}
+					id="snacks-pizza"
+					name="snacks"
+					type="checkbox"
+					value="пицца"
+				/>
+				<label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">
+					Пиццу
+				</label>
+				<input
+					className="contact-form__input contact-form__input--checkbox"
+					onChange={onChange}
+					id="snacks-cake"
+					name="snacks"
+					type="checkbox"
+					defaultChecked
+					value="пирог"
+				/>
+				<label clclassNameass="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">
+					Пирог
+				</label>
+			</div>
+			<button className="contact-form__button" onSubmit={onSubmit} type="submit">
+				Отправить сообщение!
+			</button>
+			<output id="result" />
+		</form>
+	);
 
-}
+	//   const saveForm = (event) => {
+	//     event.preventDefault();
+	//     const salutationEl = formEls.querySelector('input[type=radio]:checked');
+	//     const snacksEl = formEls.querySelector('input[type=checkbox]:checked');
+	//     let result = {
+	//       salutation: salutationEl.value,
+	//       name: nameEl.value,
+	//       subject: subjectEl.value,
+	//       message: messageEl.value,
+	//       email: emailEl.value,
+	//       snacks: snacksEl.value
+	//     }
+	//     console.log(result);
 
-// FeedbackForm.defaultProps = {
-//   salutation: 'Мисис',
-//   name: 'Алевтина',
-//   subject: 'У меня важный вопрос',
-//   message: 'Как оформить доставку?',
-//   email: 'mis@test.co',
-//   snacks: ['пирог']
-// }
-
-// function onSubmit(event)  {
-  
-// }
+	//   }
+};
 
 /******************************
  * Не вносить изменния ниже
  ******************************/
 const form = {
-  salutation: 'Мисис',
-  name: 'Алевтина',
-  subject: 'У меня важный вопрос',
-  message: 'Как оформить доставку?',
-  email: 'mis@test.co',
-  snacks: ['пирог']
+	salutation: 'Мисис',
+	name: 'Алевтина',
+	subject: 'У меня важный вопрос',
+	message: 'Как оформить доставку?',
+	email: 'mis@test.co',
+	snacks: ['пирог'],
 };
 
 function App() {
-  let output;
-  return (
-    <section className="content">
-      <h1 className="content__heading">Отправить нам сообщение</h1>
-      <p className="content__lede">Используйте эту простую форму чтобы связаться с нами.</p>
-      <FeedbackForm data={form} onSubmit={data => output.value = data} />
-      <output ref={field => output = field} />
-    </section>
-  );
+	let output;
+	return (
+		<section className="content">
+			<h1 className="content__heading">Отправить нам сообщение</h1>
+			<p className="content__lede">Используйте эту простую форму чтобы связаться с нами.</p>
+			<FeedbackForm data={form} onSubmit={data => (output.value = data)} />
+			<output ref={field => (output = field)} />
+		</section>
+	);
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
