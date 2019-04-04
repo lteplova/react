edc92231f3d2cbef21d747aa5cacf708314f6e68
Liudmila Teplova <lteplova@gmail.com>
2019-04-04 23:02:04 +0300

    acordeon done

--- composition/accordion/js/index.js
+++ composition/accordion/js/index.js
@@ -1,92 +1,79 @@
-'use strict';
+"use strict";
 
-const data = {
-    title: 'React',
-    subtitle: [
-        "Компоненты",
-        "Выучил раз, используй везде!",
-        "Использование JSX"
-    ],
-    article: [
-        "Каждый компонент являются законченной частью пользовательского интерфейса и сам управляет своим состоянием, а композиция компонентов (соединение) позволяет создавать более сложные компоненты. Таким образом, создается иерархия компонентов, причем каждый отдельно взятый компонент независим сам по себе. Такой подход позволяет строить сложные интерфейсы, где есть множество состояний, и взаимодействовать между собой.",
-        "После изучения React вы сможете использовать его концепции не только в браузере, но также и при разработке мобильных приложений с использованием React Native.",
-        "JSX является языком, расширяющим синтаксис стандартного Javascript. По факту он позволяет писать HTML-код в JS-скриптах. Такой подход упрощает разработку компонентов и повышает читаемость кода."
-    ]
-}
+const title = "React";
+const data = [
+  {
+    title: "Компоненты",
+    article:
+      "Каждый компонент являются законченной частью пользовательского интерфейса и сам управляет своим состоянием, а композиция компонентов (соединение) позволяет создавать более сложные компоненты. Таким образом, создается иерархия компонентов, причем каждый отдельно взятый компонент независим сам по себе. Такой подход позволяет строить сложные интерфейсы, где есть множество состояний, и взаимодействовать между собой."
+  },
+  {
+    title: "Выучил раз, используй везде!",
+    article:
+      "После изучения React вы сможете использовать его концепции не только в браузере, но также и при разработке мобильных приложений с использованием React Native."
+  },
+  {
+    title: "Использование JSX",
+    article:
+      "JSX является языком, расширяющим синтаксис стандартного Javascript. По факту он позволяет писать HTML-код в JS-скриптах. Такой подход упрощает разработку компонентов и повышает читаемость кода."
+  }
+];
 
-
-const Section = () => {
-    return (
-        <section className="section open">
-					<button>toggle</button>
-					<h3 className="sectionhead">{item}</h3>
-					<div className="articlewrap">
-						<div className="article">
-							Каждый компонент являются законченной частью пользовательского интерфейса и сам управляет
-							своим состоянием, а композиция компонентов (соединение) позволяет создавать более сложные
-							компоненты. Таким образом, создается иерархия компонентов, причем каждый отдельно взятый
-							компонент независим сам по себе. Такой подход позволяет строить сложные интерфейсы, где есть
-							множество состояний, и взаимодействовать между собой.
-						</div>
-					</div>
-				</section>
-    )
-
-}
-
-const Subtitle = () => {
-    return (
-
-
-    );
-}
-
-const Article = ({data}) => {data.article.map((item) => <div className="article" item={item} />)}
-    
-
-const App = ({data}) => {
-    console.log(data.subtitle);
-    
+const Section = props => {
 	return (
-			<main className="main">
-				<h2 className="title">{data.title}</h2>
-				<section className="section open">
-					<button>toggle</button>
-					<h3 className="sectionhead">{item}</h3>
-					<div className="articlewrap">
-						<div className="article">
-							Каждый компонент являются законченной частью пользовательского интерфейса и сам управляет
-							своим состоянием, а композиция компонентов (соединение) позволяет создавать более сложные
-							компоненты. Таким образом, создается иерархия компонентов, причем каждый отдельно взятый
-							компонент независим сам по себе. Такой подход позволяет строить сложные интерфейсы, где есть
-							множество состояний, и взаимодействовать между собой.
-						</div>
-					</div>
-				</section>
-				<section className="section">
-					<button>toggle</button>
-					<h3 className="sectionhead">Выучил раз, используй везде!</h3>
-					<div class="articlewrap">
-						<div className="article">
-							После изучения React вы сможете использовать его концепции не только в браузере, но также и
-							при разработке мобильных приложений с использованием React Native.
-						</div>
-					</div>
-				</section>
-				<section className="section">
-					<button>toggle</button>
-					<h3 className="sectionhead">Использование JSX</h3>
-					<div className="articlewrap">
-						<div className="article">
-							JSX является языком, расширяющим синтаксис стандартного Javascript. По факту он позволяет
-							писать HTML-код в JS-скриптах. Такой подход упрощает разработку компонентов и повышает
-							читаемость кода.
-						</div>
-					</div>
-				</section>
-			</main>
-          
-        );
-    
-	
+  <section
+    key={props.item}
+	className={`section ${props.activeItem == props.item ? "open" : ""}`}
+	onClick={e => props.handleClick(props.item)}
+  >
+    <button>toggle</button>
+    <h3 className="sectionhead">{props.title}</h3>
+    <div className="articlewrap">
+      <div className="article">{props.article}</div>
+    </div>
+  </section>
+)};
+
+const Sections = ({ data, activeItem, handleClick }) => (
+  <div>
+    {data.map((item, index) => (
+      <Section
+        item={index}
+        handleClick={handleClick}
+        activeItem={activeItem}
+        title={item.title}
+        article={item.article}
+      />
+    ))}
+  </div>
+);
+
+class App extends React.Component {
+  constructor() {
+	super();
+	this.state = {
+		activeItem: 0
+	};
+  }
+
+  handleClick = (item) => {
+	  if (this.state.activeItem == item) {
+		this.setState({activeItem: -1});
+	  } else {
+	  	this.setState({activeItem: item});
+	  }
+  };
+
+  render() {
+    return (
+      <main className="main">
+        <h2 className="title">{this.props.title}</h2>
+        <Sections
+          data={this.props.data}
+          activeItem={this.state.activeItem}
+          handleClick={this.handleClick}
+        />
+      </main>
+    );
+  }
 }
