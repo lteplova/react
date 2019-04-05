4ddd6442628418945f8244990e8f73599803ea03
Liudmila Teplova <lteplova@gmail.com>
2019-04-05 22:08:56 +0300

    form

--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -1,5 +1,44 @@
-const SubscribeForm = () => {
+class SubscribeForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isValid: true
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ isValid: event.target.validity.valid });
+  };
+
+  render() {
     return (
-        <div>Поиск</div>
-    )
-};
+      <div className="subscribe__form">
+        <form
+          className={`form form--subscribe ${
+            this.state.isValid ? "is-valid" : "is-error"
+          }`}
+        >
+          <h4 className="form-title">Подписаться:</h4>
+          <div className="form-group">
+            <label htmlFor="input-email" className="sr-only">
+              Email
+            </label>
+            <input
+              onChange={this.handleChange.bind(this)}
+              type="email"
+              id="input-email"
+              placeholder="Email"
+              className="form-control"
+            />
+            <div className="form-error">
+              Пожалуйста, проверьте корректность адреса электронной почты
+            </div>
+            <button type="submit" className="form-next">
+              <i className="material-icons">keyboard_arrow_right</i>
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
