a48608a63c76324125ce724a954ae874e2855b27
Liudmila Teplova <lteplova@gmail.com>
2019-04-05 23:16:07 +0300

    library-done

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,15 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
+const SearchBox = props => {
+  let searchString = props.value;
+  const handlerFilter = () => {
+      props.filterBooks(searchString.value);
+  };
+  return (
+    <input
+      type="text"
+      ref={el => (searchString = el)}
+      onChange={handlerFilter.bind(this)}
+      value={props.value}
+      placeholder="Поиск по названию или автору"
+    />
+  );
 };
