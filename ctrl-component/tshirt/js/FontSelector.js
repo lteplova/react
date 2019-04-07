76bf4ae2bede923a0c1987b085e54d236e3f1736
Liudmila Teplova <lteplova@gmail.com>
2019-04-07 22:23:58 +0300

    tshirt done

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,18 @@
-const FontSelector = ({fonts, selectedFont, onSelect}) => {
+const FontSelector = ({ fonts, selectedFont, onSelect }) => {
+
+  const handleCheck = (event) => {
+      onSelect(JSON.parse(event.target.value));
+  };
+
+  const fontItems = fonts.map((item) => {
     return (
-        <div className="font-picker">
-            Выберите шрифт
-        </div>
-    )
+      <div className="grid center font-item">
+        <input type="radio" name="font" id={item.name} onChange={handleCheck.bind(this)} value={JSON.stringify(item)} />
+        <label htmlFor={item.name} className="grid-1">
+            <PictureFont text={item.name.substr(0, 3)} path={item.path}/>
+        </label>
+      </div>
+    );
+  });
+  return <div className="font-picker">{fontItems}</div>;
 };
--- ctrl-component/tshirt/js/TextRenderLine.js
+++ ctrl-component/tshirt/js/TextRenderLine.js
@@ -1,7 +1,9 @@
 const TextRenderLine = ({value, onChange}) => {
+	const handlerChange = (event) => onChange(event.target.value);
+	
 	return (
 		<div className="type-text">
-			Наберите текст
+			<textarea name="text" id="font-text" cols="30" rows="2" onChange={handlerChange.bind(this)} placeholder="Введите текст для футболки">{value}</textarea>
 		</div>
 	);
 };
