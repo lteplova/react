7aa027db7a24ecc2c8296a6024c29bf2491a8060
Liudmila Teplova <lteplova@gmail.com>
2019-04-10 22:20:37 +0300

    routing task1

---
+++ routing/menu/js/Menu.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const Menu = () => (
+  <Router>
+    <nav className="menu">
+    <NavLink className="menu__item" exact activeClassName='menu__item-active' to="/">Главная</NavLink>
+    <NavLink className="menu__item" activeClassName='menu__item-active' to="/drift">Дрифт-такси</NavLink>
+    <NavLink className="menu__item" activeClassName='menu__item-active' to="/timeattack">Time Attack</NavLink>
+    <NavLink className="menu__item" activeClassName='menu__item-active' to="/forza">Forza Karting</NavLink>
+    </nav>
+  </Router>
+);
