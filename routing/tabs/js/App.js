f340e20eddc0dd7e0c2d27072c7cd7371605229f
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-04-11 10:33:03 +0300

    task2 -routing done

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,21 +1,22 @@
 'use strict';
 
-const App = () => (
-
-<Router>
-	<div className="tabs">
-	    <nav className="tabs__items">
-            <NavLink className="tabs__item" exact activeClassName="tabs__item-active" to="/">Рефераты</NavLink>
-            <NavLink className="tabs__item" activeClassName="tabs__item-active" to="/creator">Криэйтор</NavLink>
-            <NavLink className="tabs__item" activeClassName="tabs__item-active" to="/fortune">Гадалка</NavLink>
-	    </nav>
-	
-        <div className="tabs__content">
-            <Route path= "/" exact component={Essay} />
-            <Route path="/creator" exact component={Creator} />
-            <Route path="/fortune" exact component={Fortune} />
+class App extends React.Component {
+    render() {
+      return (
+        <Router>
+        <div className="tabs">
+            <nav className="tabs__items">
+                <NavLink className="tabs__item" exact activeClassName="tabs__item-active" to="/">Рефераты</NavLink>
+                <NavLink className="tabs__item" activeClassName="tabs__item-active" to="/creator">Криэйтор</NavLink>
+                <NavLink className="tabs__item" activeClassName="tabs__item-active" to="/fortune">Гадалка</NavLink>
+            </nav>
+            <div className="tabs__content">
+                <Route path= "/" exact component={Essay} />
+                <Route path="/creator" exact component={Creator} />
+                <Route path="/fortune" exact component={Fortune} />
+            </div>
         </div>
-	</div>
-</Router>
-);
-
+    </Router>
+      );
+    }
+  }
