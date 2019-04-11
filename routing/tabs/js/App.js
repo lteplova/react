52b7a644c34a87f9893d81b12fbb9b4536eb7872
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-04-11 10:29:36 +0300

    routing task2

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,21 @@
+'use strict';
+
+const App = () => (
+
+<Router>
+	<div className="tabs">
+	    <nav className="tabs__items">
+            <NavLink className="tabs__item" exact activeClassName="tabs__item-active" to="/">Рефераты</NavLink>
+            <NavLink className="tabs__item" activeClassName="tabs__item-active" to="/creator">Криэйтор</NavLink>
+            <NavLink className="tabs__item" activeClassName="tabs__item-active" to="/fortune">Гадалка</NavLink>
+	    </nav>
+	
+        <div className="tabs__content">
+            <Route path= "/" exact component={Essay} />
+            <Route path="/creator" exact component={Creator} />
+            <Route path="/fortune" exact component={Fortune} />
+        </div>
+	</div>
+</Router>
+);
 
